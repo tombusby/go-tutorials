ff9630b76c97ef726afc0425268f253b082e374c
Tom Busby <tom@flyfreeweb.co.uk>
2016-05-10 23:54:05 +0200

    Add composition examples

---
+++ 006-Composition/basic-composition.go
@@ -0,0 +1,30 @@
+
+package main
+
+import ("fmt")
+
+type Person struct {
+    Name string
+}
+
+func (p *Person) Introduce() {
+    fmt.Printf("Hi, I'm %s\n", p.Name)
+}
+
+type Saiyan struct {
+    *Person
+    Power int
+}
+
+func main() {
+    goku := &Saiyan{
+        Person: &Person{"Goku"},
+        Power: 9001,
+    }
+
+    goku.Introduce()
+
+    // Below are both equivalent
+    fmt.Println("Implicit:", goku.Name)
+    fmt.Println("Explicit:", goku.Person.Name)
+}
---
+++ 006-Composition/overloading.go
@@ -0,0 +1,33 @@
+
+package main
+
+import ("fmt")
+
+type Person struct {
+    Name string
+}
+
+func (p *Person) Introduce() {
+    fmt.Printf("Hi, I'm %s\n", p.Name)
+}
+
+type Saiyan struct {
+    *Person
+    Power int
+}
+
+func (p *Saiyan) Introduce() {
+    fmt.Printf("Hi, I'm %s. Ya!\n", p.Name)
+}
+
+func main() {
+    goku := &Saiyan{
+        Person: &Person{"Goku"},
+        Power: 9001,
+    }
+
+    goku.Introduce()
+
+    // The composed method can still be accessed explicitly
+    goku.Person.Introduce()
+}
